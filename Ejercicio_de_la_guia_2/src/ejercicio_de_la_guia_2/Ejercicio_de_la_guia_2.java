897ca1ae4c88bdd3c4655ef0a3df06aa860b8328
DxDzere <74333816+DxDzere@users.noreply.github.com>
2023-03-31 20:31:12 -0300

    Inicio ejercicio de la guia 2

---
+++ Ejercicio_de_la_guia_2/src/ejercicio_de_la_guia_2/Ejercicio_de_la_guia_2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_de_la_guia_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Zere
+ */
+public class Ejercicio_de_la_guia_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int num1, num2;
+        
+        System.out.println("Ingrese 2 numeros");
+        System.out.print("Numero 1: ");
+        num1 = leer.nextInt();
+        System.out.print("Numero 2: ");
+        num2 = leer.nextInt();
+    }
+    
+}
